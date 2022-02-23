7ea330f409831cea16f60bac9557a1d5477b9056
Neshan <neshanparvej@gmail.com>
2022-02-23 15:18:53 +0600

    small library changes

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Media } from 'reactstrap;'
+import { Media } from 'reactstrap';
 
 class Menu extends Component {
     constructor(props) {
