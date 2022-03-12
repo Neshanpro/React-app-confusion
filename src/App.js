8a6e73ebab22e87905531ed2072559c0d8ecbd36
Neshan <neshanparvej@gmail.com>
2022-03-12 22:50:13 +0600

    presentational and container components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import React, { Component } from 'react';
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
 
 class App extends Component {
+
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu />
+        <Main />
       </div>
     );
   }
