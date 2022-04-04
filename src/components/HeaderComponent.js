68c3a48b697d50573a102f5643ac07314fa909d4
Neshan <neshanparvej@gmail.com>
2022-04-04 18:43:07 +0600

    header component adding

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+
+class Header extends Component {
+    render() {
+        return (
+            <>
+                <Navbar dark>
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Jumbotron>
+                    <div className="container">
+
+                    </div>
+                </Jumbotron>
+            </>
+        )
+    }
+}
