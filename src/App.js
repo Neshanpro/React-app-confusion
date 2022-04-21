ad6953cd1caaedd980ce0f9712290fce6d0f3547
Neshan <neshanparvej@gmail.com>
2022-04-21 14:09:21 +0600

    update

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import './App.css';
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/configureStore';
-
-const store = ConfigureStore();
 
 class App extends Component {
+
   render() {
     return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div className="App">
-            <Main />
-          </div>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
