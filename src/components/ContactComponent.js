f7b7199eb4058a559fa4420075e945051c9b1c35
Neshan <neshanparvej@gmail.com>
2022-04-24 14:20:36 +0600

    min update

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,38 +1,42 @@
-import React, { Component } from 'react';
-import {
-    Breadcrumb, BreadcrumbItem, Button,
-    Label, Col, Row
-} from 'reactstrap';
+import React, {Component} from 'react';
+import { Breadcrumb, BreadcrumbItem, Button, 
+     Label,  Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, LocalForm, Form, Errors, actions } from 'react-redux-form';
 
 
-const required = (val) => val && val.length;
+//// validators
+const required = (val) => val && val.length; //value > 0
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
-const minLength = (len) => (val) => (val) && (val.length >= len);
+const minLength = (len) => (val) => (val) &&( val.length >= len);
 const isNumber = (val) => !isNaN(Number(val));
 const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 
 
-class Contact extends Component {
+class Contact extends Component{
 
-    constructor(props) {
+    constructor(props){
         super(props);
 
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit(values) {
-        console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
+    handleSubmit(values){
+        // console.log("Current State is: " + JSON.stringify( values ) );
+        // alert("Current State is: " + JSON.stringify( values ) );
+
+        
+        this.props.postFeedback( values.firstname, values.lastname, values.telnum, values.email, values.agree, values.contactType, values.message );
+        this.props.resetFeedbackForm();
 
 
+            
     }
 
 
-    render() {
+    render(){
 
         return (
             <div className="container">
@@ -45,13 +49,13 @@ class Contact extends Component {
                             Contact Us
                         </BreadcrumbItem>
                     </Breadcrumb>
-
+    
                     <div className="col-12">
                         <h3>Contact Us</h3>
                         <hr />
                     </div>
                 </div>
-
+    
                 <div className="row row-content">
                     <div className="col-12">
                         <h3>Location Information</h3>
@@ -83,19 +87,20 @@ class Contact extends Component {
                     <div className="col-12">
                         <h3> Send us your feedback</h3>
                     </div>
-
+                    
                     <div className="col-12 col-md-9">
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)} >
-
+                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)} resetOnSubmit={true}>
+                            
+                            {/* firstname */}
                             <Row className="form-group">
                                 <Label htmlFor="firstName" md={2}>First Name</Label>
                                 <Col md={10}>
-                                    <Control.text model=".firstname" id="firstname" name="firstname"
+                                    <Control.text  model=".firstname" id="firstname" name="firstname"
                                         placeholder="First Name"
-                                        className="form-control"
+                                        className="form-control"   
                                         validators={{
                                             required, minLength: minLength(3), maxLength: maxLength(15)
-                                        }}
+                                        }}                                 
                                     />
                                     <Errors
                                         className="text-danger"
@@ -110,6 +115,7 @@ class Contact extends Component {
                                 </Col>
                             </Row>
 
+                            {/* lastname */}
                             <Row className="form-group">
                                 <Label htmlFor="lastname" md={2}>Last Name</Label>
                                 <Col md={10}>
@@ -133,6 +139,7 @@ class Contact extends Component {
                                 </Col>
                             </Row>
 
+                            {/* telphone */}
                             <Row className="form-group">
                                 <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
                                 <Col md={10}>
@@ -157,6 +164,7 @@ class Contact extends Component {
                                 </Col>
                             </Row>
 
+                            {/* email */}
                             <Row className="form-group">
                                 <Label htmlFor="email" md={2}>Email</Label>
                                 <Col md={10}>
@@ -179,11 +187,12 @@ class Contact extends Component {
                                 </Col>
                             </Row>
 
+                            {/* ? */}
                             <Row className="form-group">
-                                <Col md={{ size: 6, offset: 2 }}>
+                                <Col md={{size: 6, offset: 2}}>
                                     <div className="form-check">
                                         <Label check>
-                                            <Control.checkbox model=".agree" name="agree"
+                                            <Control.checkbox model=".agree"  name="agree"
                                                 className="form-check-input"
                                             />{' '}
                                             <strong>May we contact you? </strong>
@@ -192,7 +201,7 @@ class Contact extends Component {
                                 </Col>
 
                                 <Col md={{ size: 3, offset: 1 }}>
-                                    <Control.select model=".contactType"
+                                    <Control.select model=".contactType"  
                                         className="form-control"
                                         name="contactType"
                                     >
@@ -202,31 +211,33 @@ class Contact extends Component {
                                 </Col>
                             </Row>
 
+                            {/* feedback */}
                             <Row className="form-group">
                                 <Label htmlFor="message" md={2}>Your Feedback</Label>
                                 <Col md={10}>
-                                    <Control.textarea model=".message" id="message" name="message"
+                                    <Control.textarea model=".message"  id="message" name="message"
                                         rows="12"
                                         className="form-control"
                                     />
                                 </Col>
                             </Row>
 
+                            {/* submit button */}
                             <Row className="form-group">
-                                <Col md={{ size: 10, offset: 2 }}>
+                                <Col md={{size: 10, offset: 2}}>
                                     <Button type="submit" color="primary">
                                         Send Feedback
                                     </Button>
                                 </Col>
                             </Row>
 
-                        </LocalForm>
+                        </Form>
                     </div>
 
                 </div>
             </div>
         );
-
+        
     }
 }
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
+
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import Contact from './ContactComponent';
 import About from './AboutComponent';
-import DishDetail from './DishdetailComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
-import { connect } from 'react-redux';
-import { addComment } from '../redux/ActionCreators';
+import DishDetail from './DishdetailComponent';
 
-const mapDispatchToProps = dispatch => ({
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
+import { postComment, postFeedback, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+
+const mapStateToProps = state => {
+
+    return{
+        comments: state.comments,
+        dishes: state.dishes,
+        leaders: state.leaders,
+        promotions: state.promotions,
+    }
+}       
+
+const mapDispatchToProps = (dispatch) => ({
+    postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
+    
+    
+    fetchDishes: () => { dispatch(fetchDishes()) },
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback')) },
+    
+    fetchComments: () => dispatch(fetchComments()),
+    
+    fetchPromos: () => dispatch(fetchPromos()),
+    fetchLeaders: () => dispatch(fetchLeaders()),
+    
+    
+    postFeedback: (firstname, lastname, telnum, email, agree, contactType, message) => dispatch(postFeedback(firstname, lastname, telnum, email, agree, contactType, message)),
 
 });
+
 
 class Main extends Component {
 
@@ -22,43 +49,114 @@ class Main extends Component {
         super(props);
     }
 
+    componentDidMount(){
+        this.props.fetchDishes();
+        this.props.fetchComments();
+        this.props.fetchPromos();
+        this.props.fetchLeaders();
+    }
+
     render() {
+
         const HomePage = () => {
             return (
                 <Home
-                    dish={this.props.dishes.filter((dish) => dish.featured)[0]}
-                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
-                />
-            );
-        }
+                    dish={
+                        this.props.dishes.dishes.filter(dish => dish.featured)[0]
+                    }
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishErrMess={this.props.dishes.errMess}
+                   
+                    promotion={
+                        this.props.promotions.promotions.filter(promo => promo.featured)[0]
+                    }
+                    promoLoading={this.props.promotions.isLoading}
+                    promoErrMess={this.props.promotions.errMess}
+                    
+                    leader={
+                        this.props.leaders.leaders.filter((leader) => leader.featured)[0]
+                    }
+                    leaderLoading={this.props.leaders.isLoading}
+                    leaderErrMess={this.props.leaders.errMess}
 
-        const DishWithId = ({ match }) => {
-            return (
-                <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
-                    addComment={this.props.addComment}
                 />
             );
         };
 
+
+
+
+
+
+        const AboutUsPage = () => {
+            return(
+                <About 
+                    leaders={this.props.leaders.leaders}
+                    leaderLoading={this.props.leaders.isLoading}
+                    leaderErrMess={this.props.leaders.errMess}
+                />
+            );
+        };
+
+
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    errMess={this.props.dishes.errMess}
+                    
+                    comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    commentsErrMess={this.props.comments.errMess}
+                    postComment={this.props.postComment}
+                />
+            );
+        };
+
+
+
         return (
             <div>
-                <Header />
-                <div>
-                    <Switch>
-                        <Route path='/home' component={HomePage} />
-                        <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
-                        <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
-                        <Route path='/menu/:dishId' component={DishWithId} />
-                        <Route exact path='/contactus' component={Contact} />} />
-                        <Redirect to="/home" />
-                    </Switch>
-                </div>
-                <Footer />
-            </div>
+                <Header></Header>
+
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+                        <Switch>
+                            <Route path="/home" component={ HomePage } />
+                            <Route exact path="/menu" 
+                                component={() => <Menu dishes={this.props.dishes}/> }
+                            />
+
+                            <Route path="/menu/:dishId" component={DishWithId} />
+
+                            <Route exact path="/contactus" component={() => <Contact 
+                                                                                resetFeedbackForm={this.props.resetFeedbackForm } 
+                                                                                postFeedback={this.props.postFeedback}
+
+                                                                            /> } 
+                            />
+
+                            <Route exact path="/aboutus" component={ AboutUsPage } />
+                        
+                            {/* if url dosesnt match, bydefault redirect to */}
+                            <Redirect to="/home" />
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
+
+
+                <Footer></Footer>
+            </div> 
         );
+
     }
+
 }
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
+
+
+/**
+ * 
+ * - connect(): generates a wrapper container component that 
+ *      subscribe to the store.
+ */
